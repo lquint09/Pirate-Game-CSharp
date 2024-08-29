643ccd2e1cdfd8a963978fa7bbbd4f2c48662175
yetthesket <mrlucq09@gmail.com>
2024-08-29 12:01:09 -0500

    update shop gui, it needs to be recetered

--- obj/Debug/net8.0/Pirate Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pirate Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38789ae6ed5083493a97df85c7164d55a220c6e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9867810f48de568b907bc70c8d453e9f75e4eeb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
