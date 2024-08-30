ebacbe87b3d760bc11a8a040bcc0f3cc650ab865
yetthesket <mrlucq09@gmail.com>
2024-08-30 08:30:13 -0500

    surely it didn't work

--- obj/Debug/net8.0/Pirate Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pirate Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf0d5c354d4d97ad2d01b7d9cd2094e24669f6ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52c0decb594181590536133e6128e61a42721b02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
