a6bea631d55736b8a030e89d40566a7b720c799f
yetthesket <mrlucq09@gmail.com>
2024-09-06 11:55:28 -0500

    documentation

--- Program.cs
+++ Program.cs
@@ -146,21 +146,21 @@ public async void StartGameAnimation() // animation at the start of the game
         }
         Console.Clear();
         Console.WriteLine("                   |>\n              |    |    | \n             )_)  )_)  )_)   \n            )___))___))___)\\ \n           )____)____)_____)\\ \n         _____|____|____|____\\____\n---------\\                  /---------\n^^^^^ ^^^^^  ^^^^^^   ^^^^^^^\n^^^^      ^^^    ^^^    ^^\n      ^^^^   ^^   ^^^ \n \n \n \n    Press any key to continue\n       Press 'esc' to exit");
-        await Task.Delay(1500);
+        await Task.Delay(1500); // Delay for animation effect
         if (menuanimationcancel) // check if animtion cancel token
         {
             break;
         }
         Console.Clear();
         Console.WriteLine("                    |> \n               |    |    | \n              )_)  )_)  )_)   \n             )___))___))___)\\ \n            )____)____)_____)\\ \n          _____|____|____|____\\____\n----------\\                  /---------\n^^^^^ ^^^^^   ^^^^^^ ^^^^   ^^^^^^\n^^^^      ^^^     ^^^    ^^\n      ^^^      ^^^  \n \n \n \n    Press any key to continue\n       Press 'esc' to exit");
-        await Task.Delay(1500);
-        if (menuanimationcancel)
+        await Task.Delay(1500); // Delay for animation effect
+        if (menuanimationcancel) // check if animtion cancel token
         {
             break;
         }
         Console.Clear();
         Console.WriteLine("                   |>\n              |    |    | \n             )_)  )_)  )_)   \n            )___))___))___)\\ \n           )____)____)_____)\\ \n         _____|____|____|____\\____\n---------\\                  /---------\n^^^^^ ^^^^^  ^^^^^^^^        ^^^^^\n^^^^      ^^^    ^^^    ^^\n      ^^^^   ^^   ^^^^^^^^  ^^^ \n \n \n \n    Press any key to continue\n       Press 'esc' to exit");
-        await Task.Delay(1500);
+        await Task.Delay(1500); // Delay for animation effect
         if (menuanimationcancel) // check if animation cancel toekn
         {
             break;
