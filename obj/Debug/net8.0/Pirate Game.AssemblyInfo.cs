8f4ebc3e04fd3175b887d5882b38e952c64657b4
yetthesket <mrlucq09@gmail.com>
2024-11-25 10:28:55 -0600

    L + ratio

--- obj/Debug/net8.0/Pirate Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pirate Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f92df56d302e974c567832fd991791dd8301b88d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d23b573f017a6cefd4c7cc4cc83b7208adaec0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
