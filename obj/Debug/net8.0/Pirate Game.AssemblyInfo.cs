310d026ecb6aaf79a5f087a926e7dbfcf41a932c
yetthesket <mrlucq09@gmail.com>
2025-01-09 12:30:20 -0600

    interwebs

--- obj/Debug/net8.0/Pirate Game.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pirate Game.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c77d5eafb04226029a16fff542b118d8193bf5e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3224662c783df5169f1c941d66f775fec56f0539")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pirate Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
